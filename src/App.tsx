55b41d90f4955ff73d06047bbc5b115314ff2954
Mateo Noba <mateob.nobafioriti@gmail.com>
2026-05-08 15:15:34 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Navbar } from './components/navbar';
+
+function App() {
+
+  return (
+    <div className='appPrincipal'>
+      <Navbar/>
+      <h1>Hola</h1>
+    </div>
+  )
+}
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import './navbar.css'
+
+export function Navbar(){
+    return(
+        <div className='navbar'>
+            <div className='navbarIzquierda'>
+                <p><b>Mateo Noba</b></p>
+            </div>
+            <div className='nabvarDerecha'>
+                <a href='#'>Perfil profecional</a>
+                <a href='#'>Contactos</a>
+                <a href='#'>Proyectos</a>
+            </div>
+        </div>
+    )
+}
