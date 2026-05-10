362938044242db6331108c979d9016bffe9d2394
Mateo Noba <mateob.nobafioriti@gmail.com>
2026-05-10 19:18:19 -0300

    Corrección del vite.config base mal escrita

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mi-web-persona',
+  base: '/mi-web-personal',
   plugins: [react()],
 })
