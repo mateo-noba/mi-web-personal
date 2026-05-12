8526f9d62572facd4a0e8d40a0d20e978f250adc
Mateo Noba <mateob.nobafioriti@gmail.com>
2026-05-12 15:44:02 -0300

    Se actualizo nuevamente el vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/mi-web-personal',
+  base:'/mi-web-personal/',
   plugins: [react()],
 })
